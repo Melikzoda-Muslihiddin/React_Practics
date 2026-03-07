10bcdcc2b776ce88a4c94db260d13674c5d73993
MuslimMelikzoda <muslimmelikzoda@gmail.com>
2026-03-07 08:42:35 +0500

    first commit

---
+++ hometask1/src/App.jsx
@@ -0,0 +1,21 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Pricing from "./components/Pricing";
+import Questions from "./components/Questions";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Questions />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ hometask1/src/components/Features.jsx
@@ -0,0 +1,4 @@
+function Features(){
+    return <h1>Features</h1>
+}
+export default Features
