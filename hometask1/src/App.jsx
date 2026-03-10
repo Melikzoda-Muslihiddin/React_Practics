6b4266731422edae1f7c4ecbdb18655843157d34
MuslimMelikzoda <muslimmelikzoda@gmail.com>
2026-03-10 12:13:41 +0500

    fabric commit

--- hometask1/src/App.jsx
+++ hometask1/src/App.jsx
@@ -7,16 +7,67 @@ import Footer from "./components/Footer";
 // import Card from "./components/Card";
 
 function App() {
+  const products = [
+    {
+      id: 1,
+      img: "/images/Latoflex.png",
+      name: "Латофлекс",
+      infoText:
+        "Шпон березовый лущеный, ГОСТ 99-96B Смола марки КФМТ - 15, класс эмиссии E0",
+    },
+    {
+      id: 2,
+      img: "/images/fanera.png",
+      name: "ФАНЕРА",
+      infoText: "Шпон березовый лущеный, ГОСТ 99-96В. Смола марки КФМТ - 15, класс эмиссии Е0",
+    },
+    {
+      id: 3,
+      img: "/images/Raf.png",
+      name: "БРИКЕТЫ RUF",
+      infoText: "Прессованные березовые опилки без применения химических добавоk",
+    },
+    {
+      id: 4,
+      img: "/images/Pilomaterial.png",
+      name: "ПИЛОМАТЕРИАЛЫ",
+      infoText: "Дисковый распил - ель сибирская, пихта, сосна, берёза, липа, осина",
+    },
+    {
+      id: 5,
+      img: "/images/Krovatniye.png",
+      name: "КРОВАТНЫЕ ОСНОВАНИЯ",
+      infoText: "Из берёзового шпона или из массива на ленте",
+    },
+    {
+      id: 6,
+      img: "/images/Ugol.png",
+      name: "УГОЛЬ",
+      infoText: "Микропористый высокоуглеродистый продукт",
+    },
+    {
+      id: 7,
+      img: "/images/Sajensi.png",
+      name: "САЖЕНЦЫ",
+      infoText: "Молодые деревья выращенные в питомнике",
+    },
+    {
+      id: 8,
+      img: "/images/Otxodi.png",
+      name: "ОТХОДЫ ПРОИЗВОДСТВА",
+      infoText: "Прессованные березовые опилки без применения химических добавок",
+    },
+  ];
   return (
     <div>
-    <Header />
-    <Hero />
-    <Features />
-    <Pricing />
-    <Questions />
-    <Footer />
-  </div>
-  )
+      <Header />
+      <Features products={products}/>
+      <Hero />
+      <Pricing />
+      <Questions />
+      <Footer />
+    </div>
+  );
 }
 // const App = () => {
 //   const data = [
@@ -52,23 +103,23 @@ function App() {
 //     },
 //   ];
 // return (
-  // <div>
-  //   {data.map((el, i) => {
-  //     return (
-  //       <Card
-  //         key={i}
-  //         name={el.name}
-  //         studentb={el.studentb}
-  //         studenta={el.studenta}
-  //         location={el.location}
-  //         qual={el.qual}
-  //         week={el.week}
-  //         status={el.status}
-  //       />
-  //     );
-  //   })}
-  // </div>
-  
+// <div>
+//   {data.map((el, i) => {
+//     return (
+//       <Card
+//         key={i}
+//         name={el.name}
+//         studentb={el.studentb}
+//         studenta={el.studenta}
+//         location={el.location}
+//         qual={el.qual}
+//         week={el.week}
+//         status={el.status}
+//       />
+//     );
+//   })}
+// </div>
+
 // );
 // };
 
